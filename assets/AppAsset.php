84bf58d66f20cc28ca2d281e1cc3c226ba95e614
WarLikeLaux <warlikelaux080399@mail.ru>
2026-06-03 21:55:51 +0600

    Use local Bootstrap Icons assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -2,22 +2,23 @@
 
 namespace app\assets;
 
+use yii\bootstrap5\BootstrapIconAsset;
 use yii\web\AssetBundle;
 
 /**
- * Основной набор ресурсов приложения: стили сайта и Bootstrap Icons.
+ * Основной набор ресурсов приложения.
  */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
         'css/site.css',
     ];
     public $js = [
     ];
     public $depends = [
+        BootstrapIconAsset::class,
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
     ];
